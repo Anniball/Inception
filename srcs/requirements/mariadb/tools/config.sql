f287255d4579dc576ea6deb97c77e7a789182f7e
Anniball <lucas.delmas.an123@gmail.com>
2022-09-08 13:35:53 +0200

    removed the test database cleaning which seems to be useless

--- srcs/requirements/mariadb/tools/config.sql
+++ srcs/requirements/mariadb/tools/config.sql
@@ -1,9 +1,3 @@
---Remove all just in case--
-DELETE FROM	mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-
 -- Set MySQL root password (if you don't set it no password at all)
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
 
