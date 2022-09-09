9e49397142de0c65af9820f6f0cafeb9eaf0a880
Anniball <lucas.delmas.an123@gmail.com>
2022-09-09 10:41:49 +0200

    some advancement on mariadb start

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,22 +1,37 @@
 # Give owner and group for the the DB which is normally automatically created in this folder 
-chown mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
 # Create a folder for the daemon (mysql server’s socket file)
 mkdir -p /var/run/mysqld
 #Give owner and group to that too
 chown -R mysql:mysql /var/run/mysqld
+touch /var/run/mysqld/mysqlf.pid
+mkfifo /var/run/mysqld/mysqlf.sock
 
 #Just checking if the DB has been correctly created in the right path
 if [ ! -d /var/lib/mysql/$MARIADB_DATABASE ]; then
+	echo "--Starting service"
 	service mysql start
+	#mysqld_safe &
 
+	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
+		sleep 1
+	done
 	# Execute the .sql to setup the database
-	eval "echo \"$(cat /tmp/config.sql)\"" | mariadb
+	#eval "echo \"$(cat /tmp/config.sql)\"" | mariadb -u root
+	mysql -u root -e "CREATE DATABASE $MARIADB_DATABASE";
+	mysql -u root -e "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'";
+	mysql -u root -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%'";
+	mysql -u root -e "FLUSH PRIVILEGES";
 
+	echo "--Setting password"
 	# Set MySQL root password (if you don't set it no password at all)
-	mysqladmin -u root password $MYSQL_ROOT_PASSWORD;
+	mysqladmin -u root password $MARIADB_ROOT_PASSWORD;
 
+
+	echo "--Stoping service"
+	#mysqladmin shutdow
 	service mysql stop
 fi
 
-#Start the database deaemon
+#Start the database daemon
 mysqld_safe
--- srcs/requirements/mariadb/tools/config.sql
+++ srcs/requirements/mariadb/tools/config.sql
@@ -1,11 +1,12 @@
--- Set MySQL root password (if you don't set it no password at all)
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
+--DELETE FROM mysql.user WHERE User='';
+--DELETE FROM mysql.db WHERE Db='test';
+--DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
+--SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PASSWORD');
 --Create user--
-CREATE DATABASE $MARIADB_DB;
+CREATE DATABASE '$MARIADB_DB';
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASSWORD';
 --Really not sure about that one--
 GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO $MARIADB_USER@'%';
-
 
 FLUSH PRIVILEGES;
