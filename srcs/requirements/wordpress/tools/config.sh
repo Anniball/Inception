eca64bd3e2bb394f1595f29f930dda20ed77ee82
Anniball <lucas.delmas.an123@gmail.com>
2022-09-12 14:21:43 +0200

    just added some comments in wordpress config.sh

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -3,6 +3,7 @@ grep -E "listen = 127.0.0.1" "/etc/php7/php-fpm.d/www.conf" > /dev/null 2>&1
 
 # If not found it's useless to modify it (errors will be written in /dev/null)
 if [ $? -eq 0 ]; then
+	echo "--Modifying configuration file"
 	# Replacing the listen part
 	sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" "/etc/php7/php-fpm.d/www.conf"
 
@@ -20,6 +21,7 @@ fi
 
 # Wordpress installing
 if ! wp core is-installed --allow-root; then
+	echo "--Installing Wordpress"
 	wp core install --url="$WORDPRESS_URL" \
 					--title="$WORDPRESS_TITLE" \
 					--admin_user="$WORDPRESS_ADMIN_USER" \
@@ -29,14 +31,18 @@ if ! wp core is-installed --allow-root; then
 fi
 
 # Simple update for wordpress 
+echo "--Updating wordpress"
 wp plugin update --all
 
 # Create user (check how simon does it)
+echo "--Creating example user"
 wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor \
 													--user_pass=$WORDPRESS_USER_PWD
 # Create article example 
+echo "--Creating example article"
 wp post generate --count=1 --post_title="example-post"
 
-# We need this to run wordpress but also so that the container keeps running
+# We need this FastCGI Process Manager to run wordpress but also so that the container keeps running
 # --nodaemonize == keep foreground
+echo "--Starting "
 php-fpm7 --nodaemonize
